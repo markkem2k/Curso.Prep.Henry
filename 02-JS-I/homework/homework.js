257c2e1886f3c1c768aa9decc148ab18a234f476
Marco Altamirano <marcoaltamirano231@gmail.com>
2022-03-25 20:42:50 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = _marco_;
+const nuevaString = Holda;
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 2;
